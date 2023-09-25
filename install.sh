f730fed84de48b0fa955131484d358b40bf4ee03
Andrew Rakitin <droon96@yandex.ru>
2023-09-25 12:47:12 +0300

    requi

--- install.sh
+++ install.sh
@@ -4,12 +4,21 @@ project_domain="credit-inspector.ru"
 project_path=`pwd`
 
 
-# shellcheck disable=SC2092
-# shellcheck disable=SC2006
-`$base_python_interpreter -m venv env`
+#`$base_python_interpreter -m venv env`
+#source env/bin/activate
+#pip install -U pip
+#pip install -r requirements.txt
+
+sudo apt update
+sudo apt install -y python3-pip python3-dev libpq-dev postgresql postgresql-contrib nginx curl
+
+sudo -H pip3 install --upgrade pip
+sudo -H pip3 install virtualenv
+
+virtualenv env
 source env/bin/activate
-pip install -U pip
 pip install -r requirements.txt
+
 
 sed -i "s~dbms_template_path~$project_path~g" nginx/nginx.conf systemd/gunicorn.service
 sed -i "s~dbms_template_domain~$project_domain~g" test_install/settings.py
