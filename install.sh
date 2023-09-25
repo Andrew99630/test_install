d636ad3438749e1688afaa37c67f37cfb40a81e6
Andrew Rakitin <droon96@yandex.ru>
2023-09-25 18:11:17 +0300

    requi

--- install.sh
+++ install.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-#base_python_interpreter="/usr/bin/python3.10"
-#project_domain="credit-inspector.ru"
-#project_path=`pwd`
-
-
-#`$base_python_interpreter -m venv env`
-#source env/bin/activate
-#pip install -U pip
-#pip install -r requirements.txt
 
 sudo apt update
 sudo apt install -y python3-pip python3-dev libpq-dev postgresql postgresql-contrib nginx curl
@@ -16,7 +7,7 @@ sudo -H pip3 install --upgrade pip
 sudo -H pip3 install virtualenv
 
 virtualenv env
-source env/bin/activate
+source root/test_install/env/bin/activate
 pip install -r requirements.txt
 
 sudo cp /root/test_install/systemd/gunicorn.service /etc/systemd/system/
