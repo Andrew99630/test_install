91c4970c6abe1033731cfe20cd85ef5d7adcf0f7
Andrew Rakitin <droon96@yandex.ru>
2023-09-25 12:37:52 +0300

    requi

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@ base_python_interpreter="/usr/bin/python3.10"
 project_domain="credit-inspector.ru"
 project_path=`pwd`
 
-#read -p "Python interpreter: " base_python_interpreter
-#read -p "Your domain without protocol (for example, google.com): " project_domain
+
+# shellcheck disable=SC2092
+# shellcheck disable=SC2006
 `$base_python_interpreter -m venv env`
 source env/bin/activate
 pip install -U pip
